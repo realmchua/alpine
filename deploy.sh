f52c18ad019d95eecc932b52f357866007f287d1
Realm Chua <105053122+realmchua@users.noreply.github.com>
2022-08-07 17:42:48 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,12 @@ echo '
 ╰╯╰━┻━━┻╯╰┻━┻┻┻╯╰━━╯╰━━━┻━━┻━━┻╯╰┻━━┻╯╱╰━━━┻━━┻┻━┻━━┻━━┻╯'
 
 echo "I am checking for the image file."
-result=$(docker images -q realmsg/alpine)
+result=$(cat .build)
 echo "" && echo "" && echo ""
 
 if [[ -n "$result" ]]; then
-    echo "Image found."
+    echo "Docker Image found."
+    echo "$(cat .build)"
     while true; do
 
         read -p "Do you want to proceed to deploy? (y/n) " alpine_yn
@@ -25,7 +26,7 @@ if [[ -n "$result" ]]; then
             echo Ok, we will proceed to deploy the docker image.
             echo "What shall we name the container?"
             read containerName
-            docker run -itd --name $containerName realmsg/alpine:latest
+            docker run -itd --name $containerName $(cat .build)
             break
             ;;
         [nN])
